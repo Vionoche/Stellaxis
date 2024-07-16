947c324497e0a001c1c4f4110b465f3447803c86
Artem Serednitsky <vionoche@gmail.com>
2024-07-16 14:46:58 +0300

    Refactoring

--- tests/ContentBlocks/ContentBlocks.UnitTests/Pages/PageOperationsTests.cs
+++ tests/ContentBlocks/ContentBlocks.UnitTests/Pages/PageOperationsTests.cs
@@ -32,31 +32,6 @@ public class PageOperationsTests
         page.ModificationDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromMinutes(1));
     }
     
-    [Fact]
-    public void Test_Page_Creation_And_Publication_With_Pure_Manager()
-    {
-        var pageManager = new PageManager(_dateTimeService, _domainEventHandler);
-        
-        var page = Page.Create(
-            _dateTimeService,
-            _domainEventHandler,
-            title: "Home",
-            description: "The main page",
-            seoDescription: null,
-            seoKeywords: null);
-
-        page = pageManager.Update(page, "Home", "Home page", "Welcome site", null);
-        page = pageManager.Publish(page);
-        
-        page.PageId.Should().NotBeEmpty();
-        page.Title.Should().Be("Home");
-        page.Description.Should().Be("Home page");
-        page.SeoDescription.Should().Be("Welcome site");
-        page.SeoKeywords.Should().BeNull();
-        page.CreationDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromMinutes(1));
-        page.ModificationDate.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromMinutes(1));
-    }
-    
     private readonly IDateTimeService _dateTimeService = new UtcDateTimeService();
     private readonly IDomainEventHandler _domainEventHandler = new MemoryDomainEventHandler(new UtcDateTimeService());
 }
