023ac987ad32be96a0023201218638291daff2de
Artem Serednitsky <vionoche@gmail.com>
2024-09-14 20:55:28 +0300

    Add TemplateSite project

---
+++ samples/TemplateSite/TemplateSite/TemplateSite/Program.cs
@@ -0,0 +1,40 @@
+using TemplateSite.Client.Pages;
+using TemplateSite.Components;
+
+namespace TemplateSite
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents()
+                .AddInteractiveWebAssemblyComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+            app.Run();
+        }
+    }
+}
