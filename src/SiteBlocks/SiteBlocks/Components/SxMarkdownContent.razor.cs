95b401c413d7aabfb9747f14e6f66a1a1ac49086
Artem Serednitsky <vionoche@gmail.com>
2024-09-15 23:15:21 +0300

    WIP

---
+++ src/SiteBlocks/SiteBlocks/Components/SxMarkdownContent.razor.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Markdig;
+using Microsoft.AspNetCore.Components;
+
+namespace Stellaxis.SiteBlocks.Components;
+
+public partial class SxMarkdownContent : ComponentBase
+{
+    [Parameter]
+    public string? FilePath { get; set; }
+    
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _content = await GetContentFromFragment();
+    }
+
+    private async Task<MarkupString> GetContentFromFragment()
+    {
+        
+    }
+    
+    private async Task<MarkupString> GetContentFromFile(string filePath)
+    {
+        var markdownText = await File.ReadAllTextAsync(filePath);
+        var html = Markdown.ToHtml(markdownText);
+        return new MarkupString(html);
+    }
+
+    private MarkupString _content;
+}
