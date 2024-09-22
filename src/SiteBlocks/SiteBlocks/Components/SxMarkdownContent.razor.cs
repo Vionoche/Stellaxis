19b3a106649b17f43718128bb0cb3eebda69b837
Artem Serednitsky <vionoche@gmail.com>
2024-09-22 15:36:16 +0300

    Refactoring

--- src/SiteBlocks/SiteBlocks/Components/SxMarkdownContent.razor.cs
+++ src/SiteBlocks/SiteBlocks/Components/SxMarkdownContent.razor.cs
@@ -45,18 +45,14 @@ public partial class SxMarkdownContent : ComponentBase
 
 #pragma warning disable BL0006
         
-        var markdownBuilder = framesRange.Array
+        var textContent = framesRange.Array
             .Where(x => x.FrameType is RenderTreeFrameType.Markup or RenderTreeFrameType.Text)
             .Aggregate(new StringBuilder(), (sb, frame) => sb.AppendLine(frame.TextContent));
         
 #pragma warning restore BL0006
 
-        var markdown = DetectIndentsRegex().Replace(markdownBuilder.ToString(), "\r\n");
-        
-        var markdownPipeline = new MarkdownPipelineBuilder()
-            .Build();
-        
-        var html = Markdown.ToHtml(markdown, markdownPipeline);
+        var markdown = DetectIndentsRegex().Replace(textContent.ToString(), "\r\n");
+        var html = Markdown.ToHtml(markdown);
         
         return new MarkupString(html);
     }
@@ -72,32 +68,4 @@ public partial class SxMarkdownContent : ComponentBase
 
     [GeneratedRegex(@"\r\n\s*")]
     private static partial Regex DetectIndentsRegex();
-}
-
-public static class SxMarkdownContentExtensions
-{
-    public static string GetFromSourceCodePath<T>(this T component, string filePath) 
-        where T : ComponentBase
-    {
-        var processFullPath = Environment.ProcessPath;
-        ArgumentException.ThrowIfNullOrEmpty(processFullPath, nameof(processFullPath));
-        
-        var processFileInfo = new FileInfo(processFullPath);
-        var processDirectoryPath = processFileInfo.Directory?.FullName;
-        ArgumentException.ThrowIfNullOrEmpty(processDirectoryPath, nameof(processDirectoryPath));
-        
-        var componentType = typeof(T);
-        var componentNamespace = componentType.Namespace;
-        ArgumentException.ThrowIfNullOrEmpty(componentNamespace, nameof(componentNamespace));
-        
-        var assemblyNamespace = componentType.Assembly.GetName().Name;
-        ArgumentException.ThrowIfNullOrEmpty(assemblyNamespace, nameof(assemblyNamespace));
-        
-        var deltaNamespace = componentNamespace.Substring(assemblyNamespace.Length, componentNamespace.Length - assemblyNamespace.Length);
-        var deltaPath = deltaNamespace.Replace('.', Path.DirectorySeparatorChar).TrimStart(Path.DirectorySeparatorChar);
-        
-        var result = Path.Combine(processDirectoryPath, deltaPath, filePath);
-
-        return result;
-    }
 }
---
+++ src/SiteBlocks/SiteBlocks/Components/SxMarkdownContentExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Components;
+
+namespace Stellaxis.SiteBlocks.Components;
+
+public static class SxMarkdownContentExtensions
+{
+    public static string GetFromSourceCodePath<T>(this T component, string filePath) 
+        where T : ComponentBase
+    {
+        var processFullPath = Environment.ProcessPath;
+        ArgumentException.ThrowIfNullOrEmpty(processFullPath, nameof(processFullPath));
+        
+        var processFileInfo = new FileInfo(processFullPath);
+        var processDirectoryPath = processFileInfo.Directory?.FullName;
+        ArgumentException.ThrowIfNullOrEmpty(processDirectoryPath, nameof(processDirectoryPath));
+        
+        var componentType = typeof(T);
+        var componentNamespace = componentType.Namespace;
+        ArgumentException.ThrowIfNullOrEmpty(componentNamespace, nameof(componentNamespace));
+        
+        var assemblyNamespace = componentType.Assembly.GetName().Name;
+        ArgumentException.ThrowIfNullOrEmpty(assemblyNamespace, nameof(assemblyNamespace));
+        
+        var deltaNamespace = componentNamespace.Substring(assemblyNamespace.Length, componentNamespace.Length - assemblyNamespace.Length);
+        var deltaPath = deltaNamespace.Replace('.', Path.DirectorySeparatorChar).TrimStart(Path.DirectorySeparatorChar);
+        
+        var result = Path.Combine(processDirectoryPath, deltaPath, filePath);
+
+        return result;
+    }
+}
